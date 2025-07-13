d8297e20fef38894448b7cd354bbd5dd220b51e9
charangoud333 <161558584+charangoud333@users.noreply.github.com>
2025-07-13 19:08:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import time
 from pynput.keyboard import Controller, Key
 import mediapipe as mp
 
-# Init keyboard and MediaPipe
 keyboard = Controller()
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False,
@@ -12,13 +11,11 @@ hands = mp_hands.Hands(static_image_mode=False,
                        min_tracking_confidence=0.5)
 mp_draw = mp.solutions.drawing_utils
 
-# Webcam
 cap = cv2.VideoCapture(0)
 
-# Gesture state
 gesture_locked = None
 gesture_lock_time = 0
-gesture_lock_cooldown = 1.0  # seconds
+gesture_lock_cooldown = 1.0  
 
 
 def get_gesture(landmarks, frame):
@@ -28,36 +25,29 @@ def get_gesture(landmarks, frame):
     y = lambda i: landmarks[i][2]
     x = lambda i: landmarks[i][1]
 
-    # Distance between two points
     def distance(i, j):
         return ((x(i) - x(j)) ** 2 + (y(i) - y(j)) ** 2) ** 0.5
 
-    # Finger states
     index_up = y(8) < y(6)
     middle_up = y(12) < y(10)
     ring_up = y(16) < y(14)
     pinky_up = y(20) < y(18)
 
-    # 1. Roll: all fingers folded
     if not index_up and not middle_up and not ring_up and not pinky_up:
         return "roll"
 
-    # 2. Jump: thumb and index tip close, and other fingers not fully curled
     if distance(4, 8) < 30 and (middle_up or ring_up or pinky_up):
         return "jump"
 
-    # 3. Left: only index finger up
     if index_up and not middle_up and not ring_up and not pinky_up:
         return "left"
 
-    # 4. Right: index, middle, ring up
     if index_up and middle_up and ring_up and not pinky_up:
         return "right"
 
     return None
 
 
-# Main loop
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -80,7 +70,6 @@ while True:
     current_time = time.time()
 
     if gesture != gesture_locked:
-        # New gesture or gesture disappeared
         if gesture in ["left", "right", "jump", "roll"]:
             if current_time - gesture_lock_time > gesture_lock_cooldown:
                 gesture_locked = gesture
@@ -99,14 +88,11 @@ while True:
                     keyboard.press(Key.down)
                     keyboard.release(Key.down)
     else:
-        # Same gesture, do nothing (locked)
         pass
 
-    # Unlock if hand disappears
     if gesture is None:
         gesture_locked = None
 
-    # Show gesture on screen
     cv2.putText(frame, f"Gesture: {gesture if gesture else 'None'}", (10, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
@@ -114,6 +100,5 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Cleanup
 cap.release()
 cv2.destroyAllWindows()
